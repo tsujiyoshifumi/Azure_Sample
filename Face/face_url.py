af51c6e9627e296f89ef14062dfd817c9e38a501
つじよしふみ <60136598+tsujiyoshifumi@users.noreply.github.com>
2020-06-02 17:16:22 +0900

    Faceの呼び出し部品を新規追加しました

---
+++ Face/face_url.py
@@ -0,0 +1,22 @@
+# ＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
+# 説明：画像ファイルのURLを参照してFaceAPIを呼び出します
+# 参考資料
+# https://docs.microsoft.com/ja-jp/azure/cognitive-services/face/quickstarts/python-sdk
+# ＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
+
+from azure.cognitiveservices.vision.face import FaceClient
+from msrest.authentication import CognitiveServicesCredentials
+
+# 呼び出し先のエンドポイントURLとAPIアクセスキーを指定して通信クラスをインスタンス化する
+face_client = FaceClient(
+    "エンドポイントURL",
+    CognitiveServicesCredentials("APIキー")
+    )
+
+# URLで画像を指定して呼び出しを実行する
+# メモ１：引数の値に応じて取得できるデータを変更できる
+# メモ２：戻り値は配列で返される（一つの写真に複数の顔がある可能性があるため）
+detect_faces = face_client.face.detect_with_url(
+    url="画像のURL",
+    return_face_attributes=["emotion"],
+    )
